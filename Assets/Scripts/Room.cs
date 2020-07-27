0007767385b3b81a937fe91457c6241066af27ba
Nicholas Ramsay <nicholasramsay@hotmail.com>
2020-07-27 02:29:34 -0700

    added scriptable objects

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+[CreateAssetMenu(menuName ="TextAdventure/Room")]
+public class Room : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [TextArea]
+    public string description;
+    public string roomName;
 }
